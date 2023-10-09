7e1b7e3351c64e6f8b5d7ca546d52c80a664b5fe
Samuel <sam404kim@gmail.com>
2023-10-09 10:56:11 -0700

    url

--- app/component/Axios/axios.ts
+++ app/component/Axios/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // const BASE_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000' : 'https://ghost-jobs-e674172156d1.herokuapp.com/';
 // https://theologianspen.com
-const BASE_URL = 'https://ghost-jobs-e674172156d1.herokuapp.com/'; 
+const BASE_URL = 'https://ghost-jobs-e674172156d1.herokuapp.com'; 
 export const axiosFetch = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
