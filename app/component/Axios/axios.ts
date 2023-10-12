50774aaec43a5b597b71fa7737db5c7fe945c559
Samuel <sam404kim@gmail.com>
2023-10-11 22:20:28 -0700

    added note

--- app/component/Axios/axios.ts
+++ app/component/Axios/axios.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 // https://theologianspen.com
 // const BASE_URL = 'http://localhost:5000'
 // const BASE_URL = 'https://ghost-jobs-e674172156d1.herokuapp.com'; 
+//https://www.youtube.com/watch?v=w9eOvKdk5wM
 const BASE_URL = 'https://server.ghostedon.com';
 export const axiosFetch = axios.create({
   baseURL: BASE_URL,
