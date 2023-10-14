80f817d8e6a569eb8828c7b597148c9a310896f7
Samuel <sam404kim@gmail.com>
2023-10-14 10:51:31 -0700

    adsense experiment

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,17 @@ export default function () {
   return (
     <main className='main'>
       <Banner />
-      <ins className="adsbygoogle"
+      {/*
+        <ins className="adsbygoogle"
           data-ad-client="ca-pub-2907367619848104"
           data-ad-slot="6276837611"
           data-ad-format="auto"
           data-full-width-responsive="true">
-          
-          </ins>
+        </ins>
         <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      */}
       <StoryList />
     </main>
   )
